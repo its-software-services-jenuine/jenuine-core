867ac5126e340af907d1a71f4fc8b3d7466795eb
Seubpong Monsar <pjame.fb@gmail.com>
2021-07-12 00:53:49 +0700

    Test new custom github action

--- jenuine-core-test/Utils/DBUtils.cs
+++ jenuine-core-test/Utils/DBUtils.cs
@@ -6,7 +6,7 @@ namespace Its.Jenuiue.Core.Utils
     public static class DBUtils
     {
         private static MongoDbRunner runner = MongoDbRunner.Start(
-            binariesSearchPatternOverride : "/usr/bin/*",
+            binariesSearchPatternOverride : "mongod",
             binariesSearchDirectory : "/usr/bin");
 
         private static MongoClient client = new MongoClient(runner.ConnectionString);
