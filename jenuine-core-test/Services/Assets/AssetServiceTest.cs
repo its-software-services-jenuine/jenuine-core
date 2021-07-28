575889af68756771fadd412b0e40dfe56cf18f9b
Seubpong Monsar <pjame.fb@gmail.com>
2021-07-28 10:48:27 +0700

    Added placeholder for Asset classes

---
+++ jenuine-core-test/Services/Assets/AssetServiceTest.cs
@@ -0,0 +1,11 @@
+using Xunit;
+using Its.Jenuiue.Core.Models;
+using Its.Jenuiue.Core.Utils;
+using Its.Jenuiue.Core.Models.Organization;
+
+namespace Its.Jenuiue.Core.Services.Assets
+{
+    public class AssetServiceTest
+    {
+    }
+}
