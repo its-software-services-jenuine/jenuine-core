14e28985206d7bdbadadd1e2c1aadb614e7db8fb
USER auttz <USER@DESKTOP-J5GS9R6>
2021-08-12 19:51:04 +0700

    Added IRegistration

--- jenuine-core/Services/Products/IProductsService.cs
+++ jenuine-core/Services/Products/IProductsService.cs
@@ -7,11 +7,18 @@ namespace Its.Jenuiue.Core.Services.Products
     public interface IProductsService
     {
         public void SetOrgId(string id);
+
         public List<MProduct> GetProducts(MProduct param, QueryParam queryParam);
+
         public MProduct GetProductById(MProduct param);
+
         public long GetProductsCount();
+
         public MProduct AddProduct(MProduct param);
+
         public MProduct UpdateProduct(MProduct param);
+
         public MProduct DeleteProduct(MProduct param);
+        
     }
 }
