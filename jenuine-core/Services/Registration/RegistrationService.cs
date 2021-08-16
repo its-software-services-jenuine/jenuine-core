a9845460787826f398cdce55193f8955b927c3bd
Supreeecha <auttzmargaretz@gmail.com>
2021-08-16 18:26:40 +0700

    Added RegistrationService

---
+++ jenuine-core/Services/Registration/RegistrationService.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using Its.Jenuiue.Core.Database;
+using Its.Jenuiue.Core.Models;
+using Its.Jenuiue.Core.Actions.Products;
+using Its.Jenuiue.Core.Models.Organization;
+
+namespace Its.Jenuiue.Core.Services.Registration
+{
+    public class RegistrationService
+    {
+    }
+        
+}
