f55423f0d9a2b26580612abbf26ebffac8c40598
Supreeecha <auttzmargaretz@gmail.com>
2021-09-01 18:50:22 +0700

    Added Global Model

---
+++ jenuine-core/Models/Global/MOrganize.cs
@@ -0,0 +1,10 @@
+namespace Its.Jenuiue.Core.Models.Global
+{
+    public class MOrganize : BaseGlobalModel
+    {
+        public string OrganizeId { get; set; }
+        public string OrganizeName { get; set; }
+        public string Description { get; set; }
+        
+    }
+}
